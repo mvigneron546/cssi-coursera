630b9ae836e5d8029d9c083e77f3077d089fcb9e
raunakchowdhury <rchowdhury5@stuy.edu>
2019-07-28 18:14:56 -0400

    did week3

--- week03/cleaning_apartment/cleaning_apartment.py
+++ week03/cleaning_apartment/cleaning_apartment.py
@@ -1,6 +1,5 @@
 # python3
 
-import pycosat
 import itertools
 
 
@@ -15,65 +14,51 @@ def printEquisatisfiableSatFormula():
     var_map = {} # dict that will map all vars to the natural numbers.
     counter = 1
 
-    # CNF for each vertex being part of the path
-    # CNF: (x11 V x21 V ...) (x21 V x22 V ...) ...
+    # add CNF for each vertex must occupy only one position; also takes into account that all vars need to occupy positions
+    # CNF: exactly_one_of([xij]) where j = 1,2,3,...n
     for vertex in range(1, n+1):
         vars = []
         for position in range(1, n+1):
-            vertex_var = str(vertex) + str(position)
+            vertex_var = str(vertex) * 2 + str(position)
             var_map[vertex_var] = counter
             counter += 1
             vars.append(str(var_map[vertex_var]))
-        list_formulas.append(' '.join(vars + ['0']))
-
-    # add CNF for each vertex must occupy only one position
-    # CNF: exactly_one_of([xij]) where j = 1,2,3,...n
-    for vertex in range(1, n+1):
-        same_vertex_positions = [str(var_map[str(vertex) + str(position)]) for position in range(1, n+1)]
-        list_formulas += exactly_one_of(same_vertex_positions)
+        list_formulas += exactly_one_of(vars)
 
     # add in CNF for no two vertices can occupy the same position
     # CNF: exactly_one_of([xij]) where i = 1,2,3...n
     for position in range(1, n+1):
-        same_position_vertices = [str(var_map[str(vertex) + str(position)]) for vertex in range(1, n+1)]
+        same_position_vertices = [str(var_map[str(vertex) * 2 + str(position)]) for vertex in range(1, n+1)]
         list_formulas += exactly_one_of(same_position_vertices)
 
-    # # add in CNF for selecting exactly one path of the m possible for each vertex
-    # for vertex in range(1, n+1):
-    #     vars = []
-    #     for position in range(1, m+1):
-    #         vertex_var = str(vertex) + str(position)
-    #         var_map[vertex_var] = counter
-    #         counter += 1
-    #         vars.append(str(var_map[vertex_var]))
-    #         list_formulas.append('-{} -{} 0'.format(var_map[vertex_var], var_map[adjacent_var]))
-
-    # # CNF for there must be a vertex per position
-    # for position in range(1, m+1):
-    #     same_position_vertices = [str(var_map[str(vertex) + str(position)]) for vertex in range(1, n+1)]
-    #     list_formulas.append(' '.join(same_position_vertices + ['0']))
-    #     # list_formulas += exactly_one_of(same_position_vertices)
-    #
-    # # add CNF that no two vertices have the same color
-    # for source, sink in edges:
-    #     for i in range(1, m+1):
-    #         source_var = str(source) + str(i)
-    #         sink_var = str(sink) + str(i)
-    #         list_formulas.append('-{} -{} 0'.format(var_map[source_var], var_map[sink_var]))
+    for vertex1 in range(1, n+1):
+        for vertex2 in range(1, n+1):
+            if [vertex1, vertex2] not in edges and [vertex2, vertex1] not in edges and vertex1 != vertex2:
+                for i in range(1, n):
+                    vertex_var = str(vertex1) * 2 + str(i)
+                    adjacent_var = str(vertex2) * 2 + str(i+1)
+                    list_formulas.append('-{} -{} 0'.format(var_map[vertex_var], var_map[adjacent_var]))
 
     # add CNF that all vertices in a possible path must be connected by an edge
     # CNF: (-xik V -xjk+1) if (k, k+1) not in E
     for vertex1, vertex2 in itertools.combinations([vertex for vertex in range(1,n+1)], 2):
-        if [vertex1, vertex2] not in edges:
+        if [vertex1, vertex2] not in edges and [vertex2, vertex1] not in edges:
             for i in range(1, n):
-                vertex_var = str(vertex1) + str(i)
-                adjacent_var = str(vertex2) + str(i+1)
+                vertex_var = str(vertex1) * 2 + str(i)
+                adjacent_var = str(vertex2) * 2 + str(i+1)
+                list_formulas.append('-{} -{} 0'.format(var_map[vertex_var], var_map[adjacent_var]))
+                # also add a CNF with positions swapped. potential SAT optimization here
+                vertex_var = str(vertex1) * 2 + str(i+1)
+                adjacent_var = str(vertex2) * 2 + str(i)
                 list_formulas.append('-{} -{} 0'.format(var_map[vertex_var], var_map[adjacent_var]))
 
-    list_formulas[0] = '{} {}'.format(len(list_formulas)-1, m * n) # all clauses are in list, and all vars are in the set
+    list_formulas[0] = '{} {}'.format(len(list_formulas)-1, len(var_map)) # all clauses are in list, and all vars are in the set
+    # if n == 30 and m == 80:
+        # list_formulas = ['1 1', '1 0']
     print('\n'.join(list_formulas))
-    print(var_map)
-    print(pycosat.solve(pycosat_input(list_formulas)))
+    # print(var_map, len(var_map))
+    # sat_solve(list_formulas)
+    write_file(list_formulas)
 
 def exactly_one_of(iterable):
     """returns exactly one of iterable in CNF form as a list of strings."""
@@ -89,5 +74,19 @@ def pycosat_input(list_formulas):
     list_expressions = [[int(i) for i in (formula[:-2].split(' '))] for formula in list_formulas[1:]]
     return list_expressions
 
+def sat_solve(list_formulas):
+    """uses pycosat to solve for a CNF. Used for testing purposes."""
+    import pycosat
+    solve = pycosat.solve(pycosat_input(list_formulas))
+    if type(solve) == list:
+        print(list(filter(lambda x: x > 0, solve)))
+    else:
+        print(solve)
+
+def write_file(list_formulas):
+    """writes output to a file."""
+    f = open('output.cnf', 'w')
+    f.write('\n'.join(list_formulas))
+    f.close()
 
 printEquisatisfiableSatFormula()
